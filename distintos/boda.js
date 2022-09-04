8eb1ee18e69c69e1121df50ee11d67f3ecacfd20
Marwinjr <101680339+Marwinjr@users.noreply.github.com>
2022-09-03 18:39:17 -0600

    Add files via upload

---
+++ distintos/boda.js
@@ -0,0 +1,4 @@
+
+ScrollReveal().reveal('.foto1',);
+ScrollReveal().reveal('.foto2',{ Delay: 500});
+ScrollReveal().reveal('.foto3',{ Delay: 500});
---
+++ distintos/interacion.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-bt').addEventListener('click', ()=> {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.show',);
+ScrollReveal().reveal('.a',{ Delay: 500});
+ScrollReveal().reveal('.c',{ Delay: 500});
+ScrollReveal().reveal('.d',{ Delay: 500});
+ScrollReveal().reveal('.social',{ Delay: 500});
